242c96d6cd6a6a7bf361b8ec997742c9e7b6ecab
lucascantao <cantao162@gmail.com>
2023-12-14 11:04:14 -0300

    angular project test init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesComponent,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [NgFor, NgIf],
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero:Hero): void {
+    this.selectedHero = hero;
+  }
+}
